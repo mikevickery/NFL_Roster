aa9bd115db06dac91340f84e205a3f28f07fdaa8
Mike Vickery <mvickery@trugrocer.com>
2015-09-15 21:30:23 -0600

    cards added using jQuery

--- app.js
+++ app.js
@@ -1,54 +1,56 @@
-﻿var team = [];
-
-function Player(id, name, position, number) {
-    this.id = id;
-    this.name = name;
-    this.position = position;
-    this.number = number;
-}
-
-function initialPlayerLoad() {
-    team.push(new Player("Player1","Russell Wilson", "QB", 3));
-    team.push(new Player("Player2","Reggie Bush", "RB", 23));
-    team.push(new Player("Player3","C.J. Anderson", "RB", 22));
-    team.push(new Player("Player4","Jeremy Maclin", "WR", 19));
-    team.push(new Player("Player5","Doug Baldwin", "WR", 89));
-    team.push(new Player("Player6","Jimmy Graham", "TE", 88));
-    team.push(new Player("Player7","James Casey", "TE", 80));
-    team.push(new Player("Player8","Steven Hauschka", "K", 4));
-    team.push(new Player("Player9","T.J. Ward", "ST", 43));
-    team.push(new Player("Player10","Tarvaris Jackson", "BN-QB", 7));
-    team.push(new Player("Player11","Luke Wilson", "BN-TE", 82));
-    team.push(new Player("Player12","Derrick Coleman", "BN-FB", 40));
-    team.push(new Player("Player13","Jordan Norwood", "BN-WR", 11));
-    team.push(new Player("Player14","Seth Roberts", "BN-WR", 10));
-    team.push(new Player("Player15","Marcel Reece", "BN-FB", 45));
-    team.push(new Player("Player16","Roy Helu", "BN-RB", 26));
-}
-
-initialPlayerLoad();
-
-function setIDs() {
-    var str1 = "";
-    var str2 = "";
-    var x = 0;
-    for (var i = 0; i < team.length; i++) {
-        x = i + 1;
-        str1 = "Player" + x + ".l1";
-        str2 = "Player" + x + ".l2";
-        document.getElementById(str1).innerText = team[i].name;
-        document.getElementById(str2).innerText = team[i].position + " #" + team[i].number;
-    }
-}
-
-setIDs();
+﻿//
+//function initialPlayerLoad() {
+//team.push(new Player("Russell Wilson", "QB", 3));
+//team.push(new Player("Reggie Bush", "RB", 23));
+//team.push(new Player("C.J. Anderson", "RB", 22));
+//team.push(new Player("Jeremy Maclin", "WR", 19));
+//team.push(new Player("Doug Baldwin", "WR", 89));
+//team.push(new Player("Jimmy Graham", "TE", 88));
+//team.push(new Player("James Casey", "TE", 80));
+//team.push(new Player("Steven Hauschka", "K", 4));
+//team.push(new Player("T.J. Ward", "ST", 43));
+//team.push(new Player("Tarvaris Jackson", "BN-QB", 7));
+//team.push(new Player("Luke Wilson", "BN-TE", 82));
+//team.push(new Player("Derrick Coleman", "BN-FB", 40));
+//team.push(new Player("Jordan Norwood", "BN-WR", 11));
+//team.push(new Player("Seth Roberts", "BN-WR", 10));
+//team.push(new Player("Marcel Reece", "BN-FB", 45));
+//team.push(new Player("Roy Helu", "BN-RB", 26));
+//}
+//initialPlayerLoad();
+//function setIDs() {
+//    var str1 = "";
+//    var str2 = "";
+//    var x = 0;
+//    for (var i = 0; i < team.length; i++) {
+//        x = i + 1;
+//        str1 = "Player" + x + ".l1";
+//        str2 = "Player" + x + ".l2";
+//        document.getElementById(str1).innerText = team[i].name;
+//        document.getElementById(str2).innerText = team[i].position + " #" + team[i].number;
+//    }
+//}
+//setIDs();
 
 $(document).ready(function () {
-    $(".player-card").mouseenter(function () {
-        $(this).fadeTo('slow', 0.75);
+    $(".cardtext").mouseenter(function () {
+        $(this).fadeTo('slow', 0.65);
     });
-    $(".player-card").mouseleave(function () {
+    $(".cardtext").mouseleave(function () {
         $(this).fadeTo('fast', 1);
     });
+});
+
+$("#button-add-player").on('click', function () {
+    var name = $('#playerName').val();
+    var position = $('#playerPosition').val();
+    var number = $('#playerNumber').val();
+    var html = '<div class="player-card">' +
+        '<img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" class="card2" />' +
+        '<div class="cardtext ctr">' +
+        '<div><span>' + name + '</span></div>' +
+        '<div><span>' + position + '</span></div>' +
+        '<div><span>#' + number + '</span></div></div>';    
+    $('.player-roster').prepend(html);
 });
 
