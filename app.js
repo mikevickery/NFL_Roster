e5289251eaa2df3b44e350d7bad685eb7baa0680
Mike Vickery <mvickery@trugrocer.com>
2015-10-04 11:37:18 -0600

    accordion, dropable and dragable players by position and team working

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var defaultPhoto = "images/nfl-player.gif";
 var unknownPhoto = "https://auth.cbssports.com/images/players/unknown-player-170x170.png";
 var teams = [];
+var byPositions = [];
 
 var roster = {
     players:{},
@@ -130,79 +131,6 @@ function refreshRosterSummary() {
     $("#playerTable").html(htmlTableHeader + htmlTableBody);
 }
 
-function listFullNFL2() {
-    var foundInRoster = false;
-    var htmlTable2Header = '<div class="t-row-header">' +
-        '<div class="t-row-cell"> </div>' +
-        '<div class="t-row-cell2"> </div>' +
-        '<div class="t-row-cell1"><u>Player Name</u></div>' +
-        '<div class="t-row-cell2"><u>Position</u></div>' +
-        '<div class="t-row-cell2"><u>Number</u></div>' +
-        '<div class="t-row-cell2"><u>Status</u></div>' +
-        '<div class="t-row-cell2"><u>Team</u></div>' +
-        '<div class="t-row-cell2"><u>Bye Week</u></div>' +
-        '<div class="t-row-cell2"><u>Age</u></div></div>';
-    var htmlTable2Body = "";
-    for (var id in fullNFL.players2) {
-        var player = fullNFL.players2[id];
-        if (!player) {
-
-        }
-        if (player.id % 2 === 0) {
-            htmlTable2Body = htmlTable2Body + '<div class="t-row t-row-shade">';
-        } else {
-            htmlTable2Body = htmlTable2Body + '<div class="t-row">';
-        }
-        foundInRoster = false;
-        for (var id in roster.players) {
-            var player3 = roster.players[id];
-            if (player3.name === player.name) {
-                foundInRoster = true;
-                break;
-            }
-        }
-        if (foundInRoster) {
-            htmlTable2Body = htmlTable2Body + '<div class="t-row-cell"> </div>';
-        } else {
-            htmlTable2Body = htmlTable2Body + '<div class="t-row-cell">' +
-                '<button class="btn btn-xs btn-warning ' +
-                'add-player-list" id="' + player.id + '" ' +
-                'type="submit">Add to My Roster</button></div>';
-        }
-        htmlTable2Body = htmlTable2Body + '<div class="t-row-cell2">';
-        if (!player.photo || player.photo === unknownPhoto) {
-            htmlTable2Body = htmlTable2Body + '<img src="' + defaultPhoto + '" class="card1" />';
-        } else {
-            htmlTable2Body = htmlTable2Body + '<img src="' + player.photo + '" class="card1" />';
-        }
-        htmlTable2Body = htmlTable2Body + '</div>' +
-            '<div class="t-row-cell1">' + player.name + '</div>' +
-            '<div class="t-row-cell2">' + player.position + '</div>' +
-            '<div class="t-row-cell2">';
-        if (player.number) {
-            htmlTable2Body = htmlTable2Body + player.number + '</div>';
-        } else {
-            htmlTable2Body = htmlTable2Body + ' </div>';
-        }
-        htmlTable2Body = htmlTable2Body + '<div class="t-row-cell2">' + player.status + '</div>' +
-            '<div class="t-row-cell2">' + player.team + '</div>' +
-            '<div class="t-row-cell2">' + player.byeweek + '</div>' +
-            '<div class="t-row-cell2">';
-        if (player.age) {
-            htmlTable2Body = htmlTable2Body + player.age + '</div></div>';
-        } else {
-            htmlTable2Body = htmlTable2Body + ' </div></div>';
-        }
-    }
-    $("#fullNFLlist2").html(htmlTable2Header + htmlTable2Body);
-}
-
-function removeFullNFL() {
-    $(".nfl-full-container").fadeOut('slow');
-    var htmlTable2Header = "<div></div>"
-    $("#fullNFLTable").html(htmlTable2Header);
-}
-
 // jQuery functions
 $(function () {
 
@@ -284,21 +212,15 @@ $(function () {
     });
     $('.toggle3').on('toggle', function (e, active) {
         if (active) {
-            listFullNFL2();
-            $(".container3").fadeIn('slow');
             $(".container3").scrollView();
+            $(".container3").fadeIn('slow');
+            listFullNFL2();
         } else {
             $(".container3").fadeOut('slow');
             removeFullNFL();
         }
     });
     // END TOGGLES: Getting notified of changes, and the new state:
-
-    $('#reload-default-players').on('click', initialPlayerLoad);
-    $('#reload-player-images').on('click', refreshFromAPI);
-    $('#reload-team-members').on('click', refreshTeamMembers);
-    $('#reload-fullNFL').on('click', listFullNFL2);
-    $('#remove-fullNFL').on('click', removeFullNFL);
 
 });
 
@@ -387,22 +309,152 @@ function refreshFromAPI() {
     })
 }
 
+function listTeamDetail() {
+    var h = "";
+    for (var i = 0; i < teams.length; i++) {
+        h = h + '<span class="hd1"><a href="#">' + teams[i].abbrv + ' - ' + teams[i].name + ' (' + teams[i].count + ')</a></span><div><ul>';
+        for (var m = 0; m < teams[i].members.length; m++) {
+            var player = fullNFL.players2[teams[i].members[m]];
+            h = h + '<li>' + player.name + ' ' + player.position + '</li>';
+        }
+        h = h + '</ul></div>';
+    }
+    $("#teamDetail").html(h);
+    $(function () {
+        $("#teamDetail").accordion({
+            heightStyle: "content",
+            active: false,
+            collapsible: true
+        });
+        $("#teamDetail li").draggable({
+            appendTo: "body",
+            helper: "clone"
+        });
+        //$("#addingGroup ol").droppable({
+        //    activeClass: "ui-state-default",
+        //    hoverClass: "ui-state-hover",
+        //    accept: ":not(.ui-sortable-helper)",
+        //    drop: function (event, ui) {
+        //        $(this).find(".placeholder").remove();
+        //        $("<li></li>").text(ui.draggable.text()).appendTo(this);
+        //    }
+        //}).sortable({
+        //    items: "li:not(.placeholder)",
+        //    sort: function () {
+        //        // gets added unintentionally by droppable interacting with sortable
+        //        // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
+        //        $(this).removeClass("ui-state-default");
+        //    }
+        //});
+    });
+}
+
+function listPositionDetail() {
+    var h = "";
+    for (var i = 0; i < byPositions.length; i++) {
+        h = h + '<span class="hd1"><a href="#">' + byPositions[i].description + ' (' + byPositions[i].count + ')</a></span><div><ul>';
+        for (var m = 0; m < byPositions[i].members.length; m++) {
+            var player = fullNFL.players2[byPositions[i].members[m]];
+            h = h + '<li>' + player.name + ' ' + player.team + '</li>';
+        }
+        h = h + '</ul></div>';
+    }
+    $("#positionDetail").html(h);
+    $(function () {
+        $("#positionDetail").accordion({
+            heightStyle: "content",
+            active: false,
+            collapsible: true
+        });
+        $("#positionDetail li").draggable({
+            appendTo: "body",
+            helper: "clone"
+        });
+        $("#addingGroup ol").droppable({
+            activeClass: "ui-state-default",
+            hoverClass: "ui-state-hover",
+            accept: ":not(.ui-sortable-helper)",
+            drop: function (event, ui) {
+                $(this).find(".placeholder").remove();
+                $("<li></li>").text(ui.draggable.text()).appendTo(this);
+            }
+        }).sortable({
+            items: "li:not(.placeholder)",
+            sort: function () {
+                // gets added unintentionally by droppable interacting with sortable
+                // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
+                $(this).removeClass("ui-state-default");
+            }
+        });
+    });
+}
+
 function refreshTeamMembers() {
-    for (var i = 0;i < teams.length;i++) {
+    // build team array
+    for (var i = 0; i < teams.length; i++) {
         //console.log(teams[i].abbrv+"=====================================================");
         for (var id in fullNFL.players2) {
             var player = fullNFL.players2[id];
             if (!player) {
 
             }
-            if (teams[i].abbrv === player.team) {
-                teams[i].count++;
-                teams[i].members.push(player.id);
-                //console.log(teams[i].abbrv + ":" + teams[i].name + ":" + player.team + " id:" + player.id + " n:" + player.name);
-                //console.log("Members : " + teams[i].count + " of " + teams[i].name);
+            if (player.status === "A") {
+                if (teams[i].abbrv === player.team) {
+                    teams[i].members.push(player.id);
+                    teams[i].count++;
+                    //console.log(teams[i].abbrv + ":" + teams[i].name + ":" + player.team + " id:" + player.id + " n:" + player.name);
+                    //console.log("Members : " + teams[i].count + " of " + teams[i].name);
+                }
             }
         }
     }
+    //build byPosition array
+    byPositions = [];
+    byPositions.push({ description: "Quarterback", members: [], count: 0 }); //0 in array byPositions
+    byPositions.push({ description: "Running Back", members: [], count: 0 }); //1 in array byPositions
+    byPositions.push({ description: "Wide Receiver", members: [], count: 0 }); //2 in array byPositions
+    byPositions.push({ description: "Tight End", members: [], count: 0 }); //3 in array byPositions
+    byPositions.push({ description: "Placekicker", members: [], count: 0 }); //4 in array byPositions
+    byPositions.push({ description: "Special Teams", members: [], count: 0 }); //5 in array byPositions
+    for (var id in fullNFL.players2) {
+        var player = fullNFL.players2[id];
+        if (!player) {
+
+        }
+        if (player.status === "A") {
+            switch (player.position) {
+                case "QB":
+                    byPositions[0].members.push(player.id);
+                    byPositions[0].count++;
+                    break;
+                case "RB":
+                    byPositions[1].members.push(player.id);
+                    byPositions[1].count++;
+                    break;
+                case "WR":
+                    byPositions[2].members.push(player.id);
+                    byPositions[2].count++;
+                    break;
+                case "TE":
+                    byPositions[3].members.push(player.id);
+                    byPositions[3].count++;
+                    break;
+                case "K":
+                    byPositions[4].members.push(player.id);
+                    byPositions[4].count++;
+                    break;
+                default:
+                    byPositions[5].members.push(player.id);
+                    byPositions[5].count++;
+                    break;
+            }
+        }
+    }
+    for (var i = 0; i < byPositions.length; i++) {
+        console.log("Position: " + byPositions[i].description + " count:" + byPositions[i].count);
+    }
+    listPositionDetail();
+    listTeamDetail();
 }
 
 function setDefaultPlayers(name, position, number) {
@@ -429,6 +481,91 @@ function initialPlayerLoad() {
     setDefaultPlayers("Abbrederis, Jared", "WR", "84");
     setDefaultPlayers("Abdullah, Husain", "RB", "39");
 }
+
+function listFullNFL2() {
+    var foundInRoster = false;
+    var htmlTable2Header = '<div class="t-row-header">' +
+        '<div class="t-row-cell"> </div>' +
+        '<div class="t-row-cell2"> </div>' +
+        '<div class="t-row-cell1"><u>Player Name</u></div>' +
+        '<div class="t-row-cell2"><u>Position</u></div>' +
+        '<div class="t-row-cell2"><u>Number</u></div>' +
+        '<div class="t-row-cell2"><u>Status</u></div>' +
+        '<div class="t-row-cell2"><u>Team</u></div>' +
+        '<div class="t-row-cell2"><u>Bye Week</u></div>' +
+        '<div class="t-row-cell2"><u>Age</u></div></div>';
+    var htmlTable2Body = "";
+    for (var id in fullNFL.players2) {
+        var player = fullNFL.players2[id];
+        if (!player) {
+
+        }
+        if (player.id % 2 === 0) {
+            htmlTable2Body = htmlTable2Body + '<div class="t-row t-row-shade">';
+        } else {
+            htmlTable2Body = htmlTable2Body + '<div class="t-row">';
+        }
+        foundInRoster = false;
+        for (var id in roster.players) {
+            var player3 = roster.players[id];
+            if (player3.name === player.name) {
+                foundInRoster = true;
+                break;
+            }
+        }
+        if (foundInRoster) {
+            htmlTable2Body = htmlTable2Body + '<div class="t-row-cell"> </div>';
+        } else {
+            htmlTable2Body = htmlTable2Body + '<div class="t-row-cell">' +
+                '<button class="btn btn-xs btn-warning ' +
+                'add-player-list" id="' + player.id + '" ' +
+                'type="submit">Add to My Roster</button></div>';
+        }
+        htmlTable2Body = htmlTable2Body + '<div class="t-row-cell2">';
+        if (!player.photo || player.photo === unknownPhoto) {
+            htmlTable2Body = htmlTable2Body + '<img src="' + defaultPhoto + '" class="card1" />';
+        } else {
+            htmlTable2Body = htmlTable2Body + '<img src="' + player.photo + '" class="card1" />';
+        }
+        htmlTable2Body = htmlTable2Body + '</div>' +
+            '<div class="t-row-cell1">' + player.name + '</div>' +
+            '<div class="t-row-cell2">' + player.position + '</div>' +
+            '<div class="t-row-cell2">';
+        if (player.number) {
+            htmlTable2Body = htmlTable2Body + player.number + '</div>';
+        } else {
+            htmlTable2Body = htmlTable2Body + ' </div>';
+        }
+        htmlTable2Body = htmlTable2Body + '<div class="t-row-cell2">' + player.status + '</div>' +
+            '<div class="t-row-cell2">' + player.team + '</div>' +
+            '<div class="t-row-cell2">' + player.byeweek + '</div>' +
+            '<div class="t-row-cell2">';
+        if (player.age) {
+            htmlTable2Body = htmlTable2Body + player.age + '</div></div>';
+        } else {
+            htmlTable2Body = htmlTable2Body + ' </div></div>';
+        }
+    }
+    $("#fullNFLlist2").html(htmlTable2Header + htmlTable2Body);
+}
+
+function removeFullNFL() {
+    $(".nfl-full-container").fadeOut('slow');
+    var htmlTable2Header = "<div></div>"
+    $("#fullNFLTable").html(htmlTable2Header);
+}
+
+// jQuery buttons
+$(function () {
+
+    $('#reload-default-players').on('click', initialPlayerLoad);
+    $('#reload-player-images').on('click', refreshFromAPI);
+    $('#reload-team-members').on('click', refreshTeamMembers);
+    $('#reload-fullNFL').on('click', listFullNFL2);
+    //$(document).on('click', '#reload-fullNFL', function (e) { pace.start(); });
+    $('#remove-fullNFL').on('click', removeFullNFL);
+
+});
 
 // functions done at launch...also linked to buttons in footer of container1 panel
 initialPlayerLoad();
